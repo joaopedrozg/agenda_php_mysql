778e4a0d22444033a1c0e2b1a395dfef98b3ee44
Joao Pedro Zanetti Goncalves <joao.pedrozg@hotmail.com>
2023-04-26 16:10:49 -0300

    criação do menu de navegação

--- templates/header.php
+++ templates/header.php
@@ -17,3 +17,17 @@
     <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a class="navbar-brand" href="<?=$BASE_URL ?>index.php">
+        <img src="<?=$BASE_URL ?>img/logo.svg" alt="Agenda">
+    </a>
+    <div>
+        <div class="navbar-nav">
+            <a href="<?=$BASE_URL ?>index.php" class="nav-link active" id="home-link">Agenda</a>
+            <a href="<?=$BASE_URL ?>create.php" class="nav-link active" id="home-link">Adicionar Contatos</a>
+        </div>
+    </div>
+</nav>
+<header>
+    
+</header>
