ed95002c2f1900d881ecc1581b7982abee9f2d2b
Joao Pedro Zanetti Goncalves <joao.pedrozg@hotmail.com>
2023-04-26 15:42:46 -0300

    Teste do icone da class fa fas-eye

--- index.php
+++ index.php
@@ -14,11 +14,11 @@
     <!-- Font -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- CSS -->
-    <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
 </head>
 <body>
 
 <h1>Testando </h1>
-    
+<i class="fas fa-eye"></i>
 </body>
 </html>
