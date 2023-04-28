5ac204b97a078c3ec7152bd8fc5ce61a055b8d0c
Joao Pedro Zanetti Goncalves <joao.pedrozg@hotmail.com>
2023-04-28 14:28:41 -0300

    troca do nome do arquivo css

--- templates/header.php
+++ templates/header.php
@@ -20,7 +20,7 @@
     <!-- Font -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- CSS -->
-    <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
