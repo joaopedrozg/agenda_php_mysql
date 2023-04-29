868731d259771fab43ab7419e85717c950608168
Joao Pedro Zanetti Goncalves <joao.pedrozg@hotmail.com>
2023-04-29 16:15:39 -0300

    Criação da view create e sua estilização

--- create.php
+++ create.php
@@ -3,9 +3,31 @@
     include_once("templates/header.php");
 ?>
 
-<h1>Criando contato</h1>
+<div class="container">
+    <?php include_once("templates/backbtn.html"); ?>
+    <h1 id="main-title">Criando contato</h1>
 
+    <form id="create-form" action="<?=$BASE_URL ?>config/process.php" method="post">
+        <input type="hidden" name="type" value="create">
 
+        <div class="form-group">
+            <label for="name">Nome do contato:</label>
+            <input type="text" name="name" id="name" class="form-control" placeholder="Digite o nome" required>
+        </div>
+        <div class="form-group">
+            <label for="phone">Telefone do contato:</label>
+            <input type="text" name="phone" id="phone" class="form-control" placeholder="Digite o Telefone" required>
+        </div>
+        <div class="form-group">
+            <label for="observations">Observações</label>
+            <textarea type="text" name="observations" id="observations" class="form-control" placeholder="Insira as observações" rows="3" >  </textarea>
+        </div>
+        <br>
+        <button type="submit" class="btn btn-primary">Cadastrar</button>
+
+    </form>
+
+</div>
 
 <?php
     include_once("templates/footer.php");
